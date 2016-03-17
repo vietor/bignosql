eb2bbb6e03a74ff6d82aa197d9d51821cae33d3b
Vietor Liu <vietor.liu@gmail.com>
2016-03-17 11:04:09 +0800

    Fix *keyword* wrapper support

---
+++ test/index.js
@@ -0,0 +1,141 @@
+"use strict";
+
+var should = require('should');
+var bignosql = require('../index');
+
+
+function getTestModel(client) {
+    return client.model("test", {
+        id: bignosql.NUMBER,
+        key: bignosql.STRING,
+        value: bignosql.NUMBER
+    });
+}
+
+function forInsert(context, TestModel) {
+    return function(done) {
+        TestModel.insert({
+            key: 'demo1',
+            value: 1
+        }, {
+            id: 'id'
+        }, function(err, result) {
+            should(err).be.equal(null);
+            should(result.id > 0).be.ok();
+            context.id = result.id;
+            done();
+        });
+    };
+}
+
+function forUpdate(context, TestModel) {
+    return function(done) {
+        TestModel.update({
+            id: context.id
+        }, {
+            $set: {
+                key: "demo2"
+            },
+            $inc: {
+                value: 2
+            }
+        }, function(err, rowCount) {
+            should(err).be.equal(null);
+            should(rowCount == 1).be.ok();
+            done();
+        });
+    };
+}
+
+function forFind(context, TestModel) {
+    return function(done) {
+        TestModel.find({
+            id: context.id
+        }, {
+            id: 1,
+            key: 1,
+            value: 1
+        }).sort({
+            key: -1
+        }).exec(function(err, rows) {
+            should(err).be.equal(null);
+            should(rows.length == 1).be.ok();
+            should(rows[0].key).be.eql("demo2");
+            should(rows[0].value).be.eql(3);
+            done();
+        });
+    };
+}
+
+function forRemove(context, TestModel) {
+    return function(done) {
+        TestModel.remove({
+            id: context.id
+        }, function(err, rowCount) {
+            should(err).be.equal(null);
+            should(rowCount == 1).be.ok();
+            done();
+        });
+    };
+}
+
+function forFindEmpty(context, TestModel) {
+    return function(done) {
+        TestModel.find({
+            id: context.id
+        }, function(err, rows) {
+            should(err).be.equal(null);
+            should(rows.length === 0).be.ok();
+            done();
+        });
+    };
+}
+
+
+
+describe('PostgreSQL', function() {
+
+    var client = bignosql.connect("pgsql", {
+        host: "127.0.0.1",
+        port: 5432,
+        database: "bignosql",
+        user: "vietor",
+        password: ""
+    }, {
+        debug: true
+    });
+
+    var context = {};
+    var TestModel = getTestModel(client);
+
+    it('insert', forInsert(context, TestModel));
+    it('update', forUpdate(context, TestModel));
+    it('find', forFind(context, TestModel));
+    it('remove', forRemove(context, TestModel));
+    it('find empty', forFindEmpty(context, TestModel));
+
+});
+
+
+
+describe('MySQL', function() {
+
+    var client = bignosql.connect("mysql", {
+        host: "localhost",
+        database: "bignosql",
+        user: "root",
+        password: ""
+    }, {
+        debug: true
+    });
+
+    var context = {};
+    var TestModel = getTestModel(client);
+
+    it('insert', forInsert(context, TestModel));
+    it('update', forUpdate(context, TestModel));
+    it('find', forFind(context, TestModel));
+    it('remove', forRemove(context, TestModel));
+    it('find empty', forFindEmpty(context, TestModel));
+
+});
